35eaccc97f76ebbf16fe07dc9b9149293868d8b6
Shan <shane.thiwanka@gmail.com>
2020-12-06 15:40:35 +0530

    changed logo

--- lib/screens/loadingScreen.dart
+++ lib/screens/loadingScreen.dart
@@ -5,60 +5,58 @@ import 'package:glossary/screens/home.dart';
 class LoadingScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Expanded(
-      child: Container(
-          decoration: BoxDecoration(
-            color: Colors.grey[100],
-          ),
-          padding: EdgeInsets.fromLTRB(10, 50, 10, 10),
-          child: Column(
-            children: [
-              Image(image: AssetImage('assets/logo.png')),
-              Expanded(
-                child: Align(
-                  alignment: Alignment.bottomCenter,
-                  child: Container(
-                    width: 150,
-                    height: 50,
-                    child: RaisedButton(
-                      color: Color(0xff520103),
-                      onPressed: () {
-                        Navigator.push(context,
-                            MaterialPageRoute(builder: (context) => Home()));
-                      },
-                      child: Text(
-                        'Continue',
-                        style: TextStyle(fontSize: 20, color: Colors.white),
-                      ),
+    return Container(
+        decoration: BoxDecoration(
+          color: Color(0xffffffff),
+        ),
+        padding: EdgeInsets.fromLTRB(10, 50, 10, 10),
+        child: Column(
+          children: [
+            Image(image: AssetImage('assets/logo.png')),
+            Expanded(
+              child: Align(
+                alignment: Alignment.bottomCenter,
+                child: Container(
+                  width: 150,
+                  height: 50,
+                  child: RaisedButton(
+                    color: Color(0xff520103),
+                    onPressed: () {
+                      Navigator.push(context,
+                          MaterialPageRoute(builder: (context) => Home()));
+                    },
+                    child: Text(
+                      'Continue',
+                      style: TextStyle(fontSize: 20, color: Colors.white),
                     ),
                   ),
                 ),
               ),
-              SizedBox(
-                height: 50,
+            ),
+            SizedBox(
+              height: 50,
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Main Students Union ',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 16,
+                    color: Colors.black),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  'Main Students Union ',
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 16,
-                      color: Colors.black),
-                ),
+            ),
+            Align(
+              alignment: Alignment.bottomCenter,
+              child: Text(
+                'Sabragamuwa University of Sri Lanka',
+                style: TextStyle(
+                    decoration: TextDecoration.none,
+                    fontSize: 16,
+                    color: Colors.black),
               ),
-              Align(
-                alignment: Alignment.bottomCenter,
-                child: Text(
-                  'Sabragamuwa University of Sri Lanka',
-                  style: TextStyle(
-                      decoration: TextDecoration.none,
-                      fontSize: 16,
-                      color: Colors.black),
-                ),
-              ),
-            ],
-          )),
-    );
+            ),
+          ],
+        ));
   }
 }
