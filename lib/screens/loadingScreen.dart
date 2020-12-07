ce1921343892aff53dfc1efd2e3efac751fc3ccd
VimarshanaBandara <vimarshanabandara76@gmail.com>
2020-12-07 22:24:40 +0530

    Add SplashScreen

--- lib/screens/loadingScreen.dart
+++ lib/screens/loadingScreen.dart
@@ -1,55 +1,128 @@
 import 'dart:async';
-//import 'dart:js';
 
-import 'package:flutter/widgets.dart';
 import 'package:flutter/material.dart';
-import 'package:glossary/screens/home.dart';
 
-class LoadingScreen extends StatelessWidget {
+import 'home.dart';
+
+
+
+class SplashScreen extends StatefulWidget {
+  @override
+  _SplashScreenState createState() => _SplashScreenState();
+}
+
+class _SplashScreenState extends State<SplashScreen> {
+
+  @override
+  void initState() {
+
+    super.initState();
+    displaySplash();
+  }
+
+  displaySplash(){
+
+    Timer(Duration(seconds: 4), () async{
+
+      Navigator.push(context,
+          PageRouteBuilder(
+              transitionDuration: Duration(seconds: 2),
+              transitionsBuilder: (context,animation , animationTime , child){
+                animation =CurvedAnimation(parent: animation , curve: Curves.elasticOut);
+                return ScaleTransition(
+                  scale:animation,
+                  child: child,
+                  alignment: Alignment.center,
+
+                );
+              },
+              pageBuilder: (context , animation , animationTime){
+                return Home();
+              }
+          ));
+
+
+
+    });
+
+  }
+
+
+
+
+
   @override
   Widget build(BuildContext context) {
-    return Container(
-        decoration: BoxDecoration(
-          color: Color(0xF0F0F0F0),
-        ),
-        padding: EdgeInsets.fromLTRB(10, 50, 10, 10),
-        child: Column(
-          children: [
-            Expanded(
-              child: Align(
-                alignment: Alignment.center,
-                child: Image(image: AssetImage('assets/logo.png'),
-                  height: 180,
+    return Scaffold(
+      body: Stack(
+        fit: StackFit.expand,
+        children: [
+
+          Container(
+
+            decoration: BoxDecoration(
+                image: DecorationImage(
+                  image: AssetImage('assets/img1.jpg',),  //Background Image
+                  fit: BoxFit.fill,
+                  colorFilter:
+                  ColorFilter.mode(Colors.black.withOpacity(0.3),
+                      BlendMode.dstATop),
+                ),
+                gradient: LinearGradient(
+                  colors: [Colors.red[900], Colors.orange],
+
+                )
+
+            ),
+
+          ),
+          Column(
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+
+              Expanded(
+                flex: 2,
+                child: Container(
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      CircleAvatar(
+                          backgroundColor: Colors.white,
+
+                          radius: 80.0,
+                          child: Image.asset('assets/logo.png')
+                      ),
+                      Padding(
+                        padding: EdgeInsets.only(top: 10.0),
+
+                      ),
+                      Text("Welcome",style: TextStyle(color: Colors.white,fontSize: 35.0,fontWeight: FontWeight.bold ,fontFamily:'gfont'),)
+                    ],
                   ),
+                ),
+
               ),
-            ),
-            SizedBox(
-              height: 50,
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                "Main Students' Union ",
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 16,
-                    color: Colors.black54),
-              ),
-            ),
-            Align(
-              alignment: Alignment.bottomCenter,
-              child: Text(
-                'Sabragamuwa University of \nSri Lanka',
-                textAlign: TextAlign.center,
-                style: TextStyle(
-                    decoration: TextDecoration.none,
-                    fontSize: 16,
-                    color: Colors.black54),
-              ),
-            ),
-            SizedBox(height: 10,),
-          ],
-        ));
+              Expanded(
+                flex: 1,
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+
+                    Padding(
+                      padding: EdgeInsets.only(top: 20.0),
+                    ),
+                    Padding(
+                      padding: EdgeInsets.only(right: 35.0),
+                      child:Text("Online Glossary \n         For Everyone",style: TextStyle(color: Colors.white,fontSize: 25.0,fontWeight: FontWeight.bold),) ,
+                    )
+
+                  ],
+                ),
+              )
+            ],
+          )
+        ],
+      ),
+    );
   }
 }
